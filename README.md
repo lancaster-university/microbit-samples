e8b37cb2fc67720f7d714a2c3b93950b3b171cfe
James Devine <j.devine@lancaster.ac.uk>
2016-03-24 14:51:13 +0000

    microbit-samples: Added an example using our new, componentised DAL.

---
+++ README.md
@@ -0,0 +1,3 @@
+# microbit-samples
+
+[insert documentation URL here, when we are live]
---
+++ source/main.cpp
@@ -0,0 +1,17 @@
+#include "MicroBit.h"
+
+MicroBit uBit;
+
+int main()
+{
+    // Initialise the micro:bit runtime.
+    uBit.init();
+
+    // Insert your code here!
+    uBit.display.scroll("BELLO! :)");
+
+    // If main exits, there may still be other fibers running or registered event handlers etc.
+    // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
+    // sit in the idle task forever, in a power efficient sleep.
+    release_fiber();
+}
